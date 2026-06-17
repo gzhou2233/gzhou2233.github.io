900f1e5eeca837f2098b32fb5a5ad14927085947
Red <red@cqlight.top>
2026-06-17 23:12:30 +0800

    feat: 图片点击放大 (medium-zoom)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,23 @@
-// 自定义主题 - 加载自定义样式
 import DefaultTheme from 'vitepress/theme'
+import { onMounted, watch, nextTick } from 'vue'
+import mediumZoom from 'medium-zoom'
+import { useRoute } from 'vitepress'
 import './style.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  setup() {
+    const route = useRoute()
+    
+    const initZoom = () => {
+      // 给文章中所有图片加点击放大
+      mediumZoom('.vp-doc img', { background: 'rgba(0,0,0,0.8)' })
+    }
+    
+    onMounted(() => {
+      initZoom()
+      // 页面切换时重新绑定
+      watch(() => route.path, () => nextTick(() => initZoom()))
+    })
+  }
+}
